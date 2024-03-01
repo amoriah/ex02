e7d3693e884848f0757ba70621cdbdbc5705c7a1
amoriah <yonoyko@bk.ru>
2024-03-01 22:12:07 +0300

    found example, redirect to index.html

--- auth.js
+++ auth.js
@@ -1,6 +1,5 @@
-
-
 const loginForm = document.getElementById("form");
+//https://habr.com/ru/articles/507554/
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -15,8 +14,85 @@ loginForm.addEventListener("submit", (e) => {
     password: password,
   };
 
-  var ua = new JsSIP.UA(configuration);
+  const ua = new JsSIP.UA(configuration);
 
   ua.start();
   ua.register();
+  window.location.href = "./index.html";
 });
+
+var socket = new JsSIP.WebSocketInterface("wss://voip.uiscom.ru");
+var ua = new JsSIP.UA({
+  sockets: [socket],
+  uri: "sip:0336443@voip.uiscom.ru",
+  password: "fLkFmpFFm5",
+});
+
+const remoteAudio = new window.Audio();
+remoteAudio.autoplay = true;
+
+// События регистрации клиента
+ua.on("connected", function (e) {
+  /* Ваш код */
+});
+ua.on("disconnected", function (e) {
+  /* Ваш код */
+});
+
+ua.on("registered", function (e) {
+  /* Ваш код */
+});
+ua.on("unregistered", function (e) {
+  /* Ваш код */
+});
+ua.on("registrationFailed", function (e) {
+  /* Ваш код */
+});
+
+// Обработка событии исх. звонка
+var eventHandlers = {
+  progress: function (e) {
+    console.log("call is in progress");
+
+    session.connection.ontrack = function (e) {
+      console.log(e);
+      remoteAudio.srcObject = e.streams[0];
+    };
+  },
+  failed: function (e) {
+    console.log("call failed with cause: " + e.cause);
+    $("#call").css({ display: "flex" });
+    $("#hangup").css({ display: "none" });
+  },
+  ended: function (e) {
+    console.log("call ended with cause: " + e.cause);
+    $("#call").css({ display: "flex" });
+    $("#hangup").css({ display: "none" });
+  },
+  confirmed: function (e) {
+    console.log("call confirmed");
+    console.log(e);
+  },
+};
+
+var options = {
+  eventHandlers: eventHandlers,
+  mediaConstraints: { audio: true, video: false },
+};
+
+// // Кнопка для звонка
+// $("#call").click(function (e) {
+//   session = ua.call($("#num").val(), options);
+//   $("#call").css({ display: "none" });
+//   $("#hangup").css({ display: "flex" });
+// });
+
+// // Кнопка для отбоя звонка
+// $("#hangup").click(function () {
+//   if (session) {
+//     session.terminate();
+//   }
+
+//   $("#call").css({ display: "flex" });
+//   $("#hangup").css({ display: "none" });
+// });
