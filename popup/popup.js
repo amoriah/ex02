378868d6d1ac16174fa774e773e5d7a02cc251de
amoriah <yonoyko@bk.ru>
2024-03-03 17:45:17 +0300

    move popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,5 @@
 const start = document.getElementById("start-btn");
 
 start.addEventListener("click", () => {
-  chrome.tabs.create({ url: "./index.html" });
+  chrome.tabs.create({ url: "../index.html" });
 });
